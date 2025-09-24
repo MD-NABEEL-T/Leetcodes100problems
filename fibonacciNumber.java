d8075897f5fca41380f2bb5b8067f52a11ea943e
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-09-24 22:12:56 +0530

    solved the 1 problem named fibonacci number

---
+++ fibonacciNumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class fibonacciNumber{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n ");
+        int n = sc.nextInt();
+        
+        System.out.println("The Fibonacci series is ");
+        int a=0;
+        int b=1;
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(a +" ");
+            int next = a+b ;
+             a =b;
+             b= next;
+        }
+        sc.close();        
+    }
+}
