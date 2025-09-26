46fd39eed46ca2699e92b264d911d93f21aa358d
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-09-26 17:56:18 +0530

    second problem and solved the trailing zeroes concept in factorial value

---
+++ factorialTrailingZeroes.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class factorialTrailingZeroes
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // if(n==0) return 1;
+        if(n<5) System.out.println(0);
+        else 
+        {
+            int sum =0;
+            while(n>=5)
+            {
+                n = n/5;
+                sum += n;
+            }
+            System.out.println(sum);
+        }
+    }
+
+}
+    
