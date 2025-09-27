52bbaa5281d20c078b675f1aca95ce569dd00ba8
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-09-27 15:42:22 +0530

    empty readme

--- README.md
+++
@@ -1,3 +0,0 @@
-i am Iron Man.<br>
-# Solving Leetcodes100problems
-<br> Here i will upload solutions of 100 problems from leetcodes solved in java 
---
+++ climbStairs.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class climbStairs{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    }
+}
+
+
+/*You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps*/
