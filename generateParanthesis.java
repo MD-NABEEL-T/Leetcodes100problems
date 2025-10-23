5ce02a7f5fd2211d439d457d6c519c2f3679c772
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-10-23 19:45:18 +0530

    updated

--- generateParanthesis.java
+++ generateParanthesis.java
@@ -1,22 +1,45 @@
-class Solution {
-    public List<String> generateParenthesis(int n) {
+// example 
+// List<String> res = new ArrayList<>();
+// res.add("()");
+// res.add("(())");
+
+// System.out.println(res);
+// output:[(), (())]
+
+import java.util.*;
+
+public class generateParenthesis {
+    public static void main(String[] args) {
+        int n = 3; 
+        List<String> result = generateParenthesis(n);
+        System.out.println(result);
+    }
+
+    public static List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
+        // It’s a dynamic array, meaning it grows automatically as you add more elements.
         helper(n, n, "", res);
         return res;
     }
 
-    public void helper(int left, int right, String str, List<String> res) {
-        if (left < 0 || right < 0 || left > right) return;  // invalid state
-        
-        if (left == 0 && right == 0) {  // all used up
-            res.add(str);
+    public static void helper(int left, int right, String current, List<String> res) {
+        // if invalid state
+        if (left < 0 || right < 0 || left > right) return;
+
+        // if all parentheses are used
+        if (left == 0 && right == 0) {
+            res.add(current);
             return;
         }
 
-        helper(left - 1, right, str + "(", res);  // use one '('
-        helper(left, right - 1, str + ")", res);  // use one ')'
+        // add '(' if available
+        helper(left - 1, right, current + "(", res);
+
+        // add ')' if available
+        helper(left, right - 1, current + ")", res);
     }
 }
+
 
 // close < open
 /*Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
