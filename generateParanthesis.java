8b8c7b992211aef79d40977d793f5b3fb1ac05a2
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-10-23 08:41:13 +0530

    updated

--- generateParanthesis.java
+++ generateParanthesis.java
@@ -1,7 +1,23 @@
-import java.util.*;
-public class generateParanthesis{
-    
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        helper(n, n, "", res);
+        return res;
+    }
+
+    public void helper(int left, int right, String str, List<String> res) {
+        if (left < 0 || right < 0 || left > right) return;  // invalid state
+        
+        if (left == 0 && right == 0) {  // all used up
+            res.add(str);
+            return;
+        }
+
+        helper(left - 1, right, str + "(", res);  // use one '('
+        helper(left, right - 1, str + ")", res);  // use one ')'
+    }
 }
+
 // close < open
 /*Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 Example 1:
